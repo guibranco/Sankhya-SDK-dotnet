197432609032a8a90f6d6127c44979a592b7de4b
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-01-16 10:52:53 +0400

    Add IFilterExpression

---
+++ Src/Sankhya/Helpers/IFilterExpression.cs
@@ -0,0 +1,28 @@
+﻿// ***********************************************************************
+// Assembly         : Sankhya
+// Author           : Guilherme Branco Stracini
+// Created          : 01-16-2023
+//
+// Last Modified By : Guilherme Branco Stracini
+// Last Modified On : 01-16-2023
+// ***********************************************************************
+// <copyright file="IFilterExpression.cs" company="Guilherme Branco Stracini">
+//     © 2023 Guilherme Branco Stracini. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace Sankhya.Helpers
+{
+    //TODO issue #68 (Integração Service)
+    /// <summary>
+    /// Interface IFilterExpression
+    /// </summary>
+    public interface IFilterExpression
+    {
+        /// <summary>
+        /// Builds the expression
+        /// </summary>
+        /// <returns>System.String.</returns>
+        string BuildExpression();
+    }
+}
