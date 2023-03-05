ab488da6091e2bdc5bed231605a905aafda2fa27
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-03-05 22:54:20 +0400

    Add constants file

---
+++ Src/Sankhya/SankhyaConstants.cs
@@ -0,0 +1,83 @@
+﻿namespace Sankhya;
+
+/// <summary>
+/// Class SankhyaConstants.
+/// </summary>
+internal static class SankhyaConstants
+{
+
+    /// <summary>
+    /// The session identifier cookie name
+    /// </summary>
+    public const string SessionIdCookieName = "JSESSIONID";
+
+    /// <summary>
+    /// The session manager not started
+    /// </summary>
+    public const string SessionManagerNotStarted = "Gerenciador de sessão não foi iniciado";
+
+    /// <summary>
+    /// The file not found on server
+    /// </summary>
+    public const string FileNotFoundOnServer = "O arquivo solicitado não existe no servidor";
+
+    public const string ServiceResponse = "serviceResponse";
+
+    public const string ServiceName = "serviceName";
+
+    public const string StatusMessage = "statusMessage";
+
+    public const string ResponseBody = "responseBody";
+
+    public const string Metadata = "metadata";
+
+    public const string EntitiesEn = "entities";
+
+    public const string EntityEn = "entity";
+
+    public const string EntitiesPtBr = "entidades";
+
+    public const string EntityPtBr = "entidade";
+
+    public const string InvoiceAccompaniments = "acompanhamentosNotas";
+
+    public const string Users = "usuarios";
+
+    public const string Warnings = "avisos";
+
+    public const string Messages = "mensagens";
+
+    public const string Releases = "liberacoes";
+
+    public const string Sessions = "SESSIONS";
+
+    public const string Invoices = "notas";
+
+    public const string ClientEvents = "clientEvents";
+
+    public const string CancellationResult = "resultadoCancelamento";
+
+    public const string Key = "chave";
+
+    public const string PrimaryKey = "pk";
+
+    public const string JSessionId = "jssessionid";
+
+    public const string CallId = "callID";
+
+    public const string CodeUser = "idusu";
+
+    public const string CodeUserLoggedId = "codUsuLogado";
+
+    public const string MessageUnlinkShipping = "msgDesvincularRemessa";
+
+    public const string PagerId = "pagerID";
+
+    public const string Total = "total";
+
+    public const string TotalPages = "totalPages";
+
+    public const string Name = "nome";
+
+    public const string Status = "status";
+}
