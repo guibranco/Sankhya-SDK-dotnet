3c8363cefc3968e35e892f2f04e28628a094e1cd
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-03-05 22:51:31 +0400

    Add ServiceEnvironment enumeration

---
+++ Src/Sankhya/Enums/ServiceEnvironment.cs
@@ -0,0 +1,37 @@
+﻿namespace Sankhya.Enums;
+
+using CrispyWaffle.Attributes;
+
+/// <summary>
+/// Enum ServiceEnvironment
+/// </summary>
+public enum ServiceEnvironment
+{
+    /// <summary>
+    /// The none
+    /// </summary>
+    [InternalValue("0")]
+    [HumanReadable("Nenhum")]
+    None,
+
+    /// <summary>
+    /// The production
+    /// </summary>
+    [InternalValue("8180")]
+    [HumanReadable("Produção")]
+    Production,
+
+    /// <summary>
+    /// The sandbox
+    /// </summary>
+    [InternalValue("8280")]
+    [HumanReadable("Sandbox")]
+    Sandbox,
+
+    /// <summary>
+    /// The training
+    /// </summary>
+    [InternalValue("8380")]
+    [HumanReadable("Treinamento")]
+    Training
+}
