9f01f25385930f85131c11ff0aa0194edd8383bd
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-10-08 05:33:18 +0100

    Add documentation

--- Src/Sankhya/GoodPractices/ServiceRequestUnavailableException.cs
+++ Src/Sankhya/GoodPractices/ServiceRequestUnavailableException.cs
@@ -1,4 +1,18 @@
-﻿using System.Globalization;
+﻿// ***********************************************************************
+// Assembly         : Sankhya
+// Author           : GuilhermeStracini
+// Created          : 10-07-2023
+//
+// Last Modified By : GuilhermeStracini
+// Last Modified On : 10-08-2023
+// ***********************************************************************
+// <copyright file="ServiceRequestUnavailableException.cs" company="Guilherme Branco Stracini">
+//     © 2023 Guilherme Branco Stracini. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System.Globalization;
 using System.Runtime.Serialization;
 using CrispyWaffle.Extensions;
 using Sankhya.Enums;
@@ -7,9 +21,20 @@ using Sankhya.Service;
 
 namespace Sankhya.GoodPractices;
 
+/// <summary>
+/// Class ServiceRequestUnavailableException.
+/// Implements the <see cref="ServiceRequestTemporarilyException" />.
+/// </summary>
+/// <seealso cref="ServiceRequestTemporarilyException" />
 [Serializable]
 public class ServiceRequestUnavailableException : ServiceRequestTemporarilyException
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceRequestUnavailableException"/> class.
+    /// </summary>
+    /// <param name="service">The service.</param>
+    /// <param name="request">The request.</param>
+    /// <param name="response">The response.</param>
     public ServiceRequestUnavailableException(
         ServiceName service,
         ServiceRequest request,
@@ -25,6 +50,11 @@ public class ServiceRequestUnavailableException : ServiceRequestTemporarilyExcep
             response
         ) { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceRequestUnavailableException"/> class.
+    /// </summary>
+    /// <param name="info">The information.</param>
+    /// <param name="context">The context.</param>
     protected ServiceRequestUnavailableException(SerializationInfo info, StreamingContext context)
         : base(info, context) { }
 }
