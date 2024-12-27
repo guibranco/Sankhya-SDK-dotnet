f213d3871d1af4034eacfa81f1a9160135fc7414
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-12-27 02:53:22 +0000

    refactor - clean up code and documentation formatting changes

--- Src/Sankhya/Attributes/EntityAttribute.cs
+++ Src/Sankhya/Attributes/EntityAttribute.cs
@@ -1,34 +1,33 @@
 ﻿using System;
 
-namespace Sankhya.Attributes
+namespace Sankhya.Attributes;
+
+/// <summary>
+/// Specifies that a class represents an entity with a given name.
+/// </summary>
+/// <remarks>
+/// This attribute is used to mark classes that represent entities in the Sankhya SDK.
+/// </remarks>
+/// <example>
+/// <code>
+/// [Entity("Customer")]
+/// public class Customer
+/// {
+///     // Class implementation
+/// }
+/// </code>
+/// </example>
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public sealed class EntityAttribute : Attribute
 {
     /// <summary>
-    /// Specifies that a class represents an entity with a given name.
+    /// Initializes a new instance of the <see cref="EntityAttribute"/> class with the specified name.
     /// </summary>
-    /// <remarks>
-    /// This attribute is used to mark classes that represent entities in the Sankhya SDK.
-    /// </remarks>
-    /// <example>
-    /// <code>
-    /// [Entity("Customer")]
-    /// public class Customer
-    /// {
-    ///     // Class implementation
-    /// }
-    /// </code>
-    /// </example>
-    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public sealed class EntityAttribute : Attribute
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EntityAttribute"/> class with the specified name.
-        /// </summary>
-        /// <param name="name">The name of the entity.</param>
-        public EntityAttribute(string name) => Name = name;
+    /// <param name="name">The name of the entity.</param>
+    public EntityAttribute(string name) => Name = name;
 
-        /// <summary>
-        /// Gets the name of the entity.
-        /// </summary>
-        public string Name { get; }
-    }
+    /// <summary>
+    /// Gets the name of the entity.
+    /// </summary>
+    public string Name { get; }
 }
